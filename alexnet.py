4e846ab9013ce2e73ad035732771cdd779161ddc
Sebastian Larson <sebastianlarson22@gmail.com>
2024-01-15 15:48:49 -0800

    added alexnet file boilerplate

---
+++ alexnet.py
@@ -0,0 +1,14 @@
+import tinygrad.nn as nn
+
+def main():
+    return
+
+class AlexNet:
+    def __init__(self):
+        return
+
+    def __call__(self, x):
+        return
+
+if __name__ == "__main__":
+    main()
